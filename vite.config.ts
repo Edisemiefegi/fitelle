a05f4d62621f47b0e681d8b891ca2f6c73007225
Edisemiefegi <edisemiefegi@gmail.com>
2026-09-09 12:54:20 +0100

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'node:url'
+
+export default defineConfig({
+  plugins: [vue(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
